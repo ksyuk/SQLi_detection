c3931be15a2c87e39cdb4551b927fb3250a2e447
ksyuk <kaseyuki601@gmail.com>
2024-01-09 12:49:49 +0900

    テスト関数を追加

--- utils.py
+++ utils.py
@@ -25,7 +25,7 @@ def train(network, dataloader, device, dataset_size, report_freq, hyperparams):
     loss_fn = hyperparams["loss_fn"] or nn.NLLLoss()
 
     total_loss, accuracy = 0, 0
-    count , epoch_count = 0, 0
+    count, epoch_count = 0, 0
     loss_values = []
 
     while True:
@@ -34,6 +34,8 @@ def train(network, dataloader, device, dataset_size, report_freq, hyperparams):
 
             optimizer.zero_grad()
             out = network(features)
+            if type(out) == int:
+                continue
             loss = loss_fn(out, labels)
 
             loss.backward()
@@ -60,8 +62,38 @@ def train(network, dataloader, device, dataset_size, report_freq, hyperparams):
 
     print("loss_values", loss_values)
     plt.plot(loss_values)
-    plt.xlabel('Iteration')
+    plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.show()
 
     return total_loss/count, accuracy.item()/count
+
+def test(device, model, test_loader, vocab, classes):
+    with torch.no_grad():
+        for batch_idx, (query, label) in enumerate(test_loader):
+            query, label = query.to(device), label.to(device)
+            pred = model(query)
+
+            if label == "1":
+                if pred == "1":
+                    true_positives += 1
+                else:
+                    false_negatives += 1
+                    with open('false_negatives.txt', 'a') as f:
+                        f.write(f'query: {query}\n')
+                        f.write(f'output: {pred}\n')
+                        f.write('\n')
+            else:
+                if pred == "0":
+                    true_negatives += 1
+                else:
+                    false_positives += 1
+                    with open('false_positives.txt', 'a') as f:
+                        f.write(f'query: {query}\n')
+                        f.write(f'output: {pred}\n')
+                        f.write('\n')
+
+            print(torch.argmax(pred[batch_idx]))
+            print("Actual:\nvalue={}, class_name= {}\n".format(label[batch_idx], classes[label[batch_idx]]))
+            print("Predicted:\nvalue={}, class_name= {}\n".format(pred[0].argmax(0),classes[pred[0].argmax(0)]))
+            break

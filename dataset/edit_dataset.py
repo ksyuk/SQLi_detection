523dc8f15acb3cb68cc6a3a64402378b64a2294a
ksyuk <kaseyuki601@gmail.com>
2024-01-21 12:11:02 +0900

    feat: validation.csvを作成する処理の追加

--- dataset/edit_dataset.py
+++ dataset/edit_dataset.py
@@ -9,20 +9,27 @@ def split_dataset(filename):
     dataset = load_dataset(filename)
     random.shuffle(dataset)
 
-    train = dataset[:int(len(dataset) * 0.8)]
-    test = dataset[int(len(dataset) * 0.8):]
+    train_size = int(len(dataset) * 0.6)
+    val_size = int(len(dataset) * 0.3)
+    train_dataset = dataset[:train_size]
+    val_dataset = dataset[train_size:train_size + val_size]
+    test_dataset = dataset[train_size + val_size:]
 
     with open("train.csv", 'a') as file:
         file.write("query,label\n")
-        for query, label in train:
-            # Add an extra double quote before each existing double quote
+        for query, label in train_dataset:
+            query = query.replace('"', '""')
+            file.write(f'"{query}",{label}\n')
+
+    with open("validation.csv", 'a') as file:
+        file.write("query,label\n")
+        for query, label in val_dataset:
             query = query.replace('"', '""')
             file.write(f'"{query}",{label}\n')
 
     with open("test.csv", 'a') as file:
         file.write("query,label\n")
-        for query, label in test:
-            # Add an extra double quote before each existing double quote
+        for query, label in test_dataset:
             query = query.replace('"', '""')
             file.write(f'"{query}",{label}\n')
 
@@ -61,6 +68,7 @@ def make_up_by_label(filename):
                 file.write(f'"{query}",{label}\n')
                 zero_count += 1
 
+
 # sqliv3.csvの末尾にあるカンマを削除する
 def remove_trailing_commas(filename):
     """Removes two trailing commas from each line in a file."""
@@ -74,12 +82,14 @@ def remove_trailing_commas(filename):
                 line = line[:-3] + '\n'
             file.write(line)
 
+
 if __name__ == '__main__':
     # remove_trailing_commas('./sqliv3.csv')
 
     # make_up_by_label("./dataset.csv")
 
-    count_label("./train.csv")
-    count_label("./test.csv")
-
     # split_dataset("./tmp.csv")
+
+    count_label("./train.csv")
+    count_label("./validation.csv")
+    count_label("./test.csv")
